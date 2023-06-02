209285de6ebedf97959d61a96d71288d069f0365
Freddy2 <freddy.alexis.ht@gmail.com>
2023-06-01 23:29:42 -0500

    refactor: I2D-62 edit MS-Customer and add sql-script

---
+++ cinerikuyparent/businessdomain/customer/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customer (username, password, first_name, last_name, dni, cellphone, role, enabled, has_voted)
+VALUES ('Domingo7', '$2a$10$SIF3MorTm.GeiarU.opNiOIfaBXSwCYHS.UmpmOrPrhLNBlea.v86', 'Domingo', 'Negro', '12345678', '987654321', 'customer', 't', 'f');
